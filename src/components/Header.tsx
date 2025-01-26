305b230e33f04f46c458b253662ac43ee00e6cb5
avanishranjan19941 <168275671+avanishranjan19941@users.noreply.github.com>
2025-01-26 22:54:59 +0530

    page modified

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './Header.css';
+import { FaLinkedin } from 'react-icons/fa';
 
 const Header: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,6 +14,11 @@ const Header: React.FC = () => {
       <header className="header">
         <div className="logo">
           <h1>Avanish Ranjan</h1>
+        </div>
+        <div>
+        <a href="https://www.linkedin.com/in/avanish-ranjan-2401b9192/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin size={30} color="#0077b5" />
+        </a>
         </div>
         <div className="hamburger" onClick={toggleMenu}>
           <div className="bar"></div>
