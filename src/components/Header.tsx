a0e616e40477215ee09789850e548d9f421f545d
avanishranjan19941 <168275671+avanishranjan19941@users.noreply.github.com>
2025-01-26 22:39:49 +0530

    page modified

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,36 @@
-// Header.tsx
-import React from 'react';
-import  './Header.css';
+import React, { useState } from 'react';
+import './Header.css';
 
 const Header: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <header className="header">
-    <div className="logo">
-      <h1>Avanish Ranjan - Cloud Full Stack </h1> 
-    </div>
-    <nav className="nav-links">
-      <a href="#about">About</a>
-      <a href="#projects">Projects</a>
-      <a href="#skills">Skills</a>
-      <a href="#contact">Contact</a>
-    </nav>
-  </header>
-  
+    <>
+      <header className="header">
+        <div className="logo">
+          <h1>Avanish Ranjan</h1>
+        </div>
+        <div className="hamburger" onClick={toggleMenu}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+      </header>
+
+      {/* Sidebar */}
+      <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+        <nav className="sidebar-links">
+          <a href="#about">About</a>
+          <a href="#projects">Projects</a>
+          <a href="#skills">Skills</a>
+          <a href="#contact">Contact</a>
+        </nav>
+      </div>
+    </>
   );
 };
 
